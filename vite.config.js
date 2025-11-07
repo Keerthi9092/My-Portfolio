bb1c6887db096ceaeff909b2962fb72063245486
Keerthi <nagakeerthi9092@gmail.com>
2025-11-07 16:41:31 +0530

    Deploy portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/',
-
-});
+  base: '/My-Portfolio/', // ⚠️ Must match your GitHub repo name (case-sensitive)
+})
