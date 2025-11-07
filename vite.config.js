d3c87dc5844a3c0fe4160204e94d100ff4e313c5
Keerthi <nagakeerthi9092@gmail.com>
2025-11-07 10:40:21 +0530

    Fix base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Change the base to match your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // 👈 must match your GitHub repo name
+  base: '/My-Portfolio/', 
 });
