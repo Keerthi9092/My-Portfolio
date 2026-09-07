f3453d3e57c604b6968c8713b9a65f7ec71cbaf7
Keerthi9092 <nagakeerthi9092@gmail.com>
2026-09-07 20:47:00 +0530

    Correct public image paths

--- src/App.jsx
+++ src/App.jsx
@@ -140,13 +140,13 @@ function App() {
     <div className="achievement-card">
       <p className="year">2024</p>
       <h3 className="achievement-title">AWS Certified Cloud Practitioner</h3>
-      <img src="public/images/AWS.jpg" alt="AWS Certificate" className="achievement-img" />
+      <img src="images/AWS.jpg" alt="AWS Certificate" className="achievement-img" />
     </div>
 
     <div className="achievement-card">
       <p className="year">2024</p>
       <h3 className="achievement-title">Red Hat Enterprise Application Developer Certification</h3>
-      <img src="public/images/RedHat.jpg" alt="Red Hat Certificate" className="achievement-img" />
+      <img src="images/RedHat.jpg" alt="Red Hat Certificate" className="achievement-img" />
     </div>
 
     
