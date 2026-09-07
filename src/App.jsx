1eb06a06daae447f6e864ea56308040c20bc6ca5
Keerthi9092 <nagakeerthi9092@gmail.com>
2026-09-07 20:39:40 +0530

    Fix image paths for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ function App() {
     <div className="achievement-card">
       <p className="year">2024</p>
       <h3 className="achievement-title">AWS Certified Cloud Practitioner</h3>
-      <img src="public/images/aws-certified-cloud-practitioner_badge.png" alt="AWS Certificate" className="achievement-img" />
+      <img src="public/images/AWS.jpg" alt="AWS Certificate" className="achievement-img" />
     </div>
 
     <div className="achievement-card">
